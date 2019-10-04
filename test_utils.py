40d1751efba926194aca0e7e6e77181a1c983d9b
Edman Feng <edmanfeng@hotmail.com>
2019-10-03 22:19:06 -0700

    added tests

---
+++ test_utils.py
@@ -0,0 +1,64 @@
+import utils
+
+class TestUtils:
+    def test_hamming_distance(self):
+        a = "this is a test"
+        b = "wokka wokka!!!"
+        expected = 37
+        
+        actual = utils.get_hamming_distance(
+            bytes(a, "utf-8"), bytes(b, "utf-8"))
+        assert actual == expected
+        
+    def test_transpose(self):
+        blocks = [
+            bytearray("HELL", "utf-8"),
+            bytearray("OWOR", "utf-8"),
+            bytearray("LDFO", "utf-8"),
+            bytearray("OBAR", "utf-8"),
+            bytearray("BAZ", "utf-8")]
+        
+        expected = [
+            bytearray("HOLOB", "utf-8"),
+            bytearray("EWDBA", "utf-8"),
+            bytearray("LOFAZ", "utf-8"),
+            bytearray("LROR", "utf-8")]   
+        actual = utils.transpose(blocks)
+        assert actual == expected
+        
+        blocks = [
+            bytearray("HEL", "utf-8"),
+            bytearray("LOW", "utf-8"),
+            bytearray("ORL", "utf-8"),
+            bytearray("DFO", "utf-8"),
+            bytearray("OBA", "utf-8"),
+            bytearray("RBA", "utf-8"),
+            bytearray("Z", "utf-8")]
+            
+        expected = [
+            bytearray("HLODORZ", "utf-8"),
+            bytearray("EORFBB", "utf-8"),
+            bytearray("LWLOAA", "utf-8")]
+        actual = utils.transpose(blocks)
+        assert actual == expected
+        
+    def test_make_blocks(self):
+        a = bytearray("HELLOWORLDFOOBARBAZ", "utf-8")
+        key_size = 4
+        expected = [
+            bytearray("HELL", "utf-8"),
+            bytearray("OWOR", "utf-8"),
+            bytearray("LDFO", "utf-8"),
+            bytearray("OBAR", "utf-8")]
+            
+        assert utils.make_blocks(a, key_size) == expected
+        
+        key_size = 3
+        expected = [
+            bytearray("HEL", "utf-8"),
+            bytearray("LOW", "utf-8"),
+            bytearray("ORL", "utf-8"),
+            bytearray("DFO", "utf-8"),
+            bytearray("OBA", "utf-8"),
+            bytearray("RBA", "utf-8")]
+        assert utils.make_blocks(a, key_size) == expected
