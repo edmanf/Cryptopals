c0f363639bf6550624a89e79a424d15afe99b8b6
Edman Feng <edmanfeng@hotmail.com>
2019-10-14 02:38:02 -0700

    wrote encrypt and stub for decrypt

--- KVParser.py
+++ KVParser.py
@@ -16,6 +16,19 @@ class KVParser:
         result = "".join(chunks)
         return result[:-1] # removes fencepost &
         
+    def encrypt(self, aes_key):
+        """ Returns an aes encrypyted string of this KVParser.
+        Decrypting with the given key and passing the decrypted string
+        to the KVParser constructor will rebuild the parser. """
+        
+    @classmethod
+    def decrypt_profile(cls, ciphertext, aes_key):
+        """ Decrypts the ciphertext with aes_key and returns a KVParser
+        built with the resulting plaintext. """
+        
+        # Need to remove potential padding from plaintext
+        return None
+        
         
     @classmethod
     def profile_for(cls, email_address):
