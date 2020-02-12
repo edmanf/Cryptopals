90ba9395ca37e75a5859602d33892074018d5826
edmanf <edmanfeng@hotmail.com>
2020-02-12 14:44:47 -0800

    moved tests to own package and created utility file open function to facilitate opening resources after the move

--- tests/test_set2.py
+++ tests/test_set2.py
@@ -3,6 +3,8 @@ import convert
 import aes
 from KVParser import KVParser
 import profile_for_attack as pfa
+from utils import res_file_open
+
 
 class TestSet2:
     def test_c9(self):
@@ -12,121 +14,117 @@ class TestSet2:
         expected = "YELLOW SUBMARINE\x04\x04\x04\x04"
         actual = utils.PKCS7_pad(message, pad_length)
         assert actual.decode() == expected
-        
+
         message = bytearray("YELLOW SUBMARINE", "utf-8")
         pad_length = 16
         expected = "YELLOW SUBMARINE"
         actual = utils.PKCS7_pad(message, pad_length)
         assert actual.decode() == expected
-        
+
         message = bytearray("HELLO WORLD", "utf-8")
         pad_length = 16
         expected = "HELLO WORLD\x05\x05\x05\x05\x05"
         actual = utils.PKCS7_pad(message, pad_length)
         assert actual.decode() == expected
-        
+
     def test_c10(self):
         """ Implement CBC mode. """
-        input_file = "res/10.txt"
-        expected_file = "res/10_expected.txt"
-        
-        f = open(expected_file)
+        input_file = "10.txt"
+        expected_file = "10_expected.txt"
+
+        f = res_file_open(expected_file)
         expected = f.read()
         f.close()
-        
-        f = open(input_file)
+
+        f = res_file_open(input_file)
         b64_text = f.read()
         f.close()
-        
-        input = convert.b64_string_to_bytes(b64_text)
+
+        input_pt = convert.b64_string_to_bytes(b64_text)
         key = bytearray("YELLOW SUBMARINE", "utf-8")
         iv = bytes("\x00", "ascii") * len(key)
-        actual = aes.aes_cbc_decrypt(input, key, iv)
+        actual = aes.aes_cbc_decrypt(input_pt, key, iv)
         assert actual.decode() == expected
-        
-        
+
     def test_c11(self):
         """ An ECB/CBC detection oracle. """
         plaintext = bytearray("A", "utf-8") * 256
-        result = aes.ecb_cbc_encryption_oracle(plaintext, 
-            mode = aes.Mode.ECB)
+        result = aes.ecb_cbc_encryption_oracle(plaintext,
+                                               mode=aes.Mode.ECB)
         mode = aes.detect_aes_encryption_mode(result.ciphertext, 16)
         assert mode == aes.Mode.ECB
-        
-        result = aes.ecb_cbc_encryption_oracle(plaintext, 
-            mode = aes.Mode.CBC)
-        mode = aes.detect_aes_encryption_mode(result.ciphertext, 16) 
+
+        result = aes.ecb_cbc_encryption_oracle(plaintext,
+                                               mode=aes.Mode.CBC)
+        mode = aes.detect_aes_encryption_mode(result.ciphertext, 16)
         assert mode == aes.Mode.CBC
-        
+
     def test_c12(self):
         """ Byte-at-a-time ECB decryption (Simple) """
         expected = aes.get_unknown_string_c12().decode()
-        
+
         assert aes.simple_ecb_oracle_decryption().decode() == expected
-        
+
     def test_c13(self):
         profile = pfa.profile_attack()
         assert profile.get(bytes("role", "utf-8")) == bytes("admin", "utf-8")
-        
-        
+
+
 class TestMisc:
     def test_detect_oracle_block_size(self):
         key_len = 16
         key = aes.get_rand_aes_key(key_len)
-        
+
         actual = aes.detect_ecb_oracle_block_size(key)
         assert actual == key_len
-        
+
         key_len = 32
         key = aes.get_rand_aes_key(key_len)
-        
+
         actual = aes.detect_ecb_oracle_block_size(key)
         assert actual == key_len
-        
+
     def test_ecb_oracle_encryption_mode(self):
         key = aes.get_rand_aes_key(16)
         pt = bytearray("A", "utf-8") * 256
         expected = aes.Mode.ECB
-        
+
         actual = aes.detect_aes_encryption_mode(aes.simple_ecb_oracle(pt, key), len(key))
         assert actual == expected
-        
+
     def test_kvparser(self):
         args = bytearray("foo=HELLO&bar=WORLD", "utf-8")
         assert KVParser(args).get_repr() == args
-        
+
         args = bytearray("foo=HELLO", "utf-8")
         assert KVParser(args).get_repr() == args
-        
+
     def test_profile_for(self):
-        input = bytearray("foo@bar.com", "utf-8")
+        input_email = bytearray("foo@bar.com", "utf-8")
         expected = "email=foo@bar.com&uid=10&role=user"
-        assert KVParser.profile_for(input).to_string() == expected
-        
+        assert KVParser.profile_for(input_email).to_string() == expected
+
     def test_email_sanitizer(self):
-        input = bytearray("foo@bar.com&role=admin", "utf-8")
+        input_email = bytearray("foo@bar.com&role=admin", "utf-8")
         expected_sanitize = bytearray("foo@bar.comroleadmin", "utf-8")
-        assert KVParser.sanitize_email(input) == expected_sanitize
-    
+        assert KVParser.sanitize_email(input_email) == expected_sanitize
+
         expected = bytearray(
-            "email=foo@bar.comroleadmin&uid=10&role=user", 
+            "email=foo@bar.comroleadmin&uid=10&role=user",
             "utf-8")
-        assert KVParser.profile_for(input).to_string() == expected.decode()
-        
-    def test_KVParser_enc_dec(self):
-        input = bytearray("foo@bar.com", "utf-8")
+        assert KVParser.profile_for(input_email).to_string() == expected.decode()
+
+    def test_kvparser_enc_dec(self):
+        input_email = bytearray("foo@bar.com", "utf-8")
         expected = "email=foo@bar.com&uid=10&role=user"
         key = bytearray("YELLOW SUBMARINE", "utf-8")
-        
-        ct = KVParser.profile_for(input).encrypt(key)
+
+        ct = KVParser.profile_for(input_email).encrypt(key)
         parser = KVParser.decrypt_profile(ct, key)
         assert parser.to_string() == expected
-        
-        input = bytearray("foo@bar.com&role=admin", "utf-8")
+
+        input_email = bytearray("foo@bar.com&role=admin", "utf-8")
         expected = "email=foo@bar.comroleadmin&uid=10&role=user"
-        ct = KVParser.profile_for(input).encrypt(key)
+        ct = KVParser.profile_for(input_email).encrypt(key)
         parser = KVParser.decrypt_profile(ct, key)
         assert parser.to_string() == expected
-        
-        
-        
