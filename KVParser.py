f736effd7ff448804e374a56c5229788dac336a5
edmanf <edmanfeng@hotmail.com>
2020-02-12 16:05:17 -0800

    refactoring for pep8

--- KVParser.py
+++ KVParser.py
@@ -1,86 +1,81 @@
 import utils
 import aes
 
+
 class KVParser:
-    def __init__(self, args, encoding = "utf-8"):
+    def __init__(self, args, encoding="utf-8"):
         self.args = {}
         self.encoding = encoding
-        
+
         for arg in args.split(bytes("&", encoding)):
             key, value = arg.split(bytes("=", encoding))
             self.args[bytes(key)] = value
-    
+
     def get(self, key):
         return self.args[key]
-        
+
     def get_repr(self):
         result = bytearray()
         for key in self.args:
             result += key + \
-                bytes("=", self.encoding) + \
-                self.args[bytes(key)] + \
-                bytes("&", self.encoding)
-        return result[:-1] # removes fencepost &
-        
+                      bytes("=", self.encoding) + \
+                      self.args[bytes(key)] + \
+                      bytes("&", self.encoding)
+        return result[:-1]  # removes fencepost &
+
     def to_string(self):
         return self.get_repr().decode()
-        
+
     def encrypt(self, aes_key):
         """ Returns an aes encrypyted string of this KVParser.
         Decrypting with the given key and passing the decrypted string
         to the KVParser constructor will rebuild the parser. """
         padded = utils.PKCS7_pad(
-            bytearray(self.to_string(), self.encoding), 
+            bytearray(self.to_string(), self.encoding),
             len(aes_key))
         return aes.aes_ecb_encrypt(padded, aes_key)
-        
-        
+
     @classmethod
     def decrypt_profile(cls, ciphertext, aes_key):
         """ Decrypts the ciphertext with aes_key and returns a KVParser
         built with the resulting plaintext. """
-        
+
         # Need to remove potential padding from plaintext
         plaintext = aes.aes_ecb_decrypt(ciphertext, aes_key)
-        
+
         block_size = len(aes_key)
         # remove padding
         for i in range(block_size):
             index = len(plaintext) - 1 - i
-            if ((plaintext[index] < ord("Z") and plaintext[index] > ord("A")) or 
-                (plaintext[index] < ord("z") and plaintext[index] > ord("a"))):
+            if ((ord("Z") > plaintext[index] > ord("A")) or
+                    (ord("z") > plaintext[index] > ord("a"))):
                 return KVParser(plaintext[:index + 1])
         return KVParser(plaintext)
-        
-        
+
     @classmethod
-    def profile_for(cls, email_address, encoding = "utf-8"):
+    def profile_for(cls, email_address, encoding="utf-8"):
         """ Creates a parser for the user profile with the given
         email_address. email_address must be a string. """
         sanitized = KVParser.sanitize_email(email_address)
         uid = bytes("10", encoding)
         role = (bytes("user", encoding))
-        
-        input = bytearray("email=", encoding) + \
+
+        args = bytearray("email=", encoding) + \
             sanitized + \
             bytes("&uid=", encoding) + \
             uid + \
             bytes("&role=", encoding) + \
             role
-        
-        return KVParser(input)
-        
-        
-        
-    @classmethod    
-    def sanitize_email(cls, email_adress, encoding = "utf-8"):
+
+        return KVParser(args)
+
+    @classmethod
+    def sanitize_email(cls, email_address, encoding="utf-8"):
         """ Returns the email address with "=" and "&" characters
         removed. """
         result = bytearray()
-        for c in email_adress:
+        for c in email_address:
             if c is not ord("=") and c is not ord("&"):
                 result.append(c)
 
         return result
-        
-    
--- profile_for_attack.py
+++ profile_for_attack.py
@@ -2,13 +2,13 @@ from KVParser import KVParser
 import aes
 import utils
 
+
 def profile_attack():
     key = aes.get_rand_aes_key(16)
-    
 
     base = KVParser.profile_for(bytearray()).encrypt(key)
     min_length = len(base)
-    
+
     block_size = 0
     for i in range(1, 512):
         junk_bytes = bytearray("A", "utf-8") * i
@@ -17,30 +17,20 @@ def profile_attack():
             block_size = length - min_length
             break
     num_pre_bytes = block_size - len("email=")
-    pre_bytes = bytearray("A", "utf-8") * num_pre_bytes # aligns payload
+    pre_bytes = bytearray("A", "utf-8") * num_pre_bytes  # aligns payload
     payload = utils.PKCS7_pad(bytearray("admin", "utf-8"), block_size)
-    post_bytes = bytearray("B", "utf-8") * (len("user") - 1) # room for &
-    
+    post_bytes = bytearray("B", "utf-8") * (len("user") - 1)  # room for &
+
     input = pre_bytes + payload + post_bytes
-    
+
     ct = bytearray(KVParser.profile_for(input).encrypt(key))
     test = KVParser.decrypt_profile(ct, key)
     ct_payload = ct[block_size:2 * block_size]
     result = ct[:-1 * block_size] + ct_payload
-    
+
     return KVParser.decrypt_profile(result, key)
-    
-    
-    
-    # input = X random junk bytes + ("admin" + padding)
-    
-    # insert 3 more bytes because you want "user" in its own block
-    # one from 
-    
-    # input = X + ("admin" + padding) + Y
-    
-    # replace last block of ct with the ("admin" + padding) block
-    
+
+
 if __name__ == '__main__':
     profile = profile_attack()
     print(profile.to_string())
