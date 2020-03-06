b858e94fd85a959536d055ecb0b6cf664b8fc761
edmanf <edmanfeng@hotmail.com>
2020-03-05 17:03:42 -0800

    c16: cbc bitflipping encrypt done

--- cbc_bitflipping.py
+++ cbc_bitflipping.py
@@ -7,7 +7,8 @@ key = aes.get_rand_aes_key(16)
 def encrypt(input_string):
     prepend_string = b"comment1=cooking%20MCs;userdata="
     append_string = b";comment2=%20like%20a%20pound%20of%20bacon"
-    return None
+    string = prepend_string + bytes(sanitize(input_string)) + append_string
+    return aes.aes_cbc_encrypt(string, key)
 
 
 def sanitize(input_string):
