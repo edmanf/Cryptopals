0316f80a749c968785630f7a198d1970349303bf
Ed <Ed@DESKTOP-IC7SM7T>
2023-01-16 02:17:28 -0800

    Added unit tests for convert.py

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -3,11 +3,32 @@ import unittest
 from src.cryptopals import convert
 
 class TestConvert(unittest.TestCase):
+
+    def setUp(self):
+        self.hex_string = "deadbeef"
+        self.hex_bytes = b"\xde\xad\xbe\xef"
+        self.b64 = b"3q2+7w=="
+        
+    
     def test_hex_string_to_bytes(self):
-        hex_string = "deadbeef"
-        expected = b"\xde\xad\xbe\xef"
-        bytes_output = convert.hex_string_to_bytes(hex_string)
-        self.assertEqual(bytes_output, expected)
+        expected = self.hex_bytes
+        actual = convert.hex_string_to_bytes(self.hex_string)
+        self.assertEqual(actual, expected)
+        
+    def test_hex_to_base64(self):
+        expected = self.b64
+        actual = convert.hex_to_b64(self.hex_bytes)
+        self.assertEqual(actual, expected)
+        
+    def test_hex_string_to_b64(self):
+        expected = self.b64
+        actual = convert.hex_string_to_b64(self.hex_string)
+        self.assertEqual(actual, expected)
+        
+    def test_b64_string_to_hex(self):
+        expected = self.hex_bytes
+        actual = convert.b64_string_to_bytes("3q2+7w==")
+        self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
     unittest.main()
